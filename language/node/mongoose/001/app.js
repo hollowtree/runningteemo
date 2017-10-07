cf1bbb2628e581bc0ab19ec1a1c45428f0a80f63
hollowtree <wangjianshu1994@gmail.com>
2017-10-07 11:48:18 +0800

    try mongoose

---
+++ language/node/mongoose/001/app.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+    console.log('open!')
+})
+
+var kittySchema = mongoose.Schema({
+    name: String
+},{
+    autoIndex: true,
+    timestamps:{
+        createAt: 'created_at'
+    }
+})
+kittySchema.methods.speak =function () {
+    var greeting  = this.name? 'Meow name is ' + this.name :'I don\'t have a name'
+    console.log(greeting)
+}
+var kitten = mongoose.model('Kitten', kittySchema)
+
+// var silence = new kitten({ name: 'Teemo' })
+
+
+
+
+// silence.save(function (err, fluffy) {
+//     if(err) return console.error(err)
+//     silence.speak();
+// })
+
+
+// kitten.find(function (err,kittens) {
+//     if(err) return console.error(err)
+//     console.log(kittens)
+// })
+
+kitten.find({name:/^T/}, function (err,kittens) {
+    if(err) return console.log(err)
+    console.log(kittens[1].createdAt);
+    
+})
