2b3eebe1cd9a00b3149f6fde3582ce32df8a25da
hollowtree <wangjianshu1994@gmail.com>
2017-10-17 23:00:53 +0800

    use node send email

---
+++ language/node/email/001/app.js
@@ -0,0 +1,46 @@
+'use strict';
+const nodemailer = require('nodemailer');
+const account = require('./account.config')
+
+let mailOptions = {
+    from: '"smalltree" <account.user>', // sender address
+    to: account.to, // list of receivers
+    subject: '新消息提醒', // Subject line
+    text: '最新消息：近来可好?Pooled connections have TCP Keep-Alive enabled for them, but servers may still close idle connections, in which case they will be removed from the pool and a new connection will be made when a new HTTP request is made for that host and port. Servers may also refuse to allow multiple requests over the same connection, in which case the connection will have to be remade for every request and cannot be pooled. The Agent will still make the requests to that server, but each one will occur over a new connection.', // plain text body
+    // html: '<b>你好啊，好久不见</b>' // html body
+};
+
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+nodemailer.createTestAccount((err, account) => {
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'QQ',
+        host: 'smtp.qq.com',
+        secureConnection: true, // use SSL
+        // // port: 587,
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: account.user, // generated ethereal user
+            pass: account.pass  // generated ethereal password
+        }
+    });
+
+    // setup email data with unicode symbols
+
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+});
