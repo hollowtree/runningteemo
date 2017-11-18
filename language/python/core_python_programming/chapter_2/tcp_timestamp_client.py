4998e9df49c7d8adf74f199b1a8dbf18962f5bf1
hollowtree <wangjianshu1994@gmail.com>
2017-11-18 19:32:57 +0800

    tcp timestamp client & server

---
+++ language/python/core_python_programming/chapter_2/tcp_timestamp_client.py
@@ -0,0 +1,19 @@
+from socket import *
+HOST = '127.0.0.1' or 'localhost'
+PORT = 8080
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    tcpCliSock.send(bytes(data, 'utf-8'))
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+tcpCliSock.close()
---
+++ language/python/core_python_programming/chapter_2/tcp_timestamp_server.py
@@ -0,0 +1,24 @@
+from socket import *
+from time import ctime
+HOST = ''
+PORT = 8080
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+while True:
+    print('waiting for connection ...')
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('... connected from: ', addr)
+
+    while True:
+        data = tcpCliSock.recv(BUFSIZ)
+        if not data:
+            break
+        tcpCliSock.send(bytes('[ % s] % s' %
+                              (ctime(), data.decode('utf-8')), 'utf-8'))
+    tcpCliSock.close()
+tcpSerSock.close()
