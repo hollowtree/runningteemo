e0772659b8d37e35720c7461d3fd06f827dddc33
hollowtree <wangjianshu1994@gmail.com>
2017-11-24 00:17:49 +0800

    bar chart (with div element)

--- language/js/try_lib/d3/basic/app/001.js
+++ language/js/try_lib/d3/basic/app/001.js
@@ -1,10 +1,22 @@
-import * as d3 from 'd3';
+import * as d3 from 'd3'
 
-var data = [2, 3, 6, 8, 9, 23, 67]
+    ; (function () {
+        var data = [2, 3, 6, 8, 9, 23, 67]
+        var x = d3.scaleLinear()
+            .domain([0, d3.max(data)])
+            .range([0, 800])
 
-d3.select('.chart1')
-    .selectAll('div')
-    .data(data)
-    .enter().append('div')
-    .style('width', function (d) { return d * 10 + 'px' })
-    .text(function (d) { return d })
+        d3.select("body")
+            .append('div')
+            .attr('class', 'chart chart1')
+            .selectAll('div')
+            .data(data)
+            .enter().append('div')
+            .style('width', '12px')
+            .transition()
+            .duration(1000)
+            // .delay(function (d, i) { console.log(d, i); return i * 100 })
+            // --- .style('width', function (d) { return d * 10 + 'px' })
+            .style('width', function (d) { return x(d) + 'px' })
+            .text(function (d) { return d })
+    })()
