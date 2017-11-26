1f04885903814437d1aa243a8b515e088be141b9
hollowtree <wangjianshu1994@gmail.com>
2017-11-26 18:57:34 +0800

    d3 chart

--- language/js/try_lib/d3/basic/app/001.js
+++ language/js/try_lib/d3/basic/app/001.js
@@ -1,6 +1,144 @@
 import * as d3 from 'd3'
 
+    ; (function () {
+        var data = [20, 20, 20]
+        var width = 500,
+            height = 500,
+            margin = 25,
+            axisLength = width - 2 * margin
 
+        var xScale = d3.scaleLinear()
+            .domain([0, 100])
+            .range([0, axisLength])
+
+        var yScale = d3.scaleLinear()
+            .domain([0, 100])
+            .range([0, axisLength])
+
+        var chart = d3.select('body')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .style('border', '1px solid #666')
+
+        chart.append('g')
+            .classed('x-axis', true)
+            .attr('transform', function () {
+                return 'translate(' + margin + ',' + (height - margin) + ')'
+            })
+            .call(d3.axisBottom(xScale))
+
+        chart.selectAll('g.x-axis g.tick')
+            .append('line')
+            .classed('grid-line', true)
+            .attr('x1', 0)
+            .attr('y1', 0)
+            .attr('x2', 0)
+            .attr('y2', -(height - 2 * margin))
+            .style('stroke-width', 1)
+            .style('stroke', '#ccc')
+
+        chart.append('g')
+            .classed('y-axis', true)
+            .attr('transform', function () {
+                return 'translate(' + margin + ',' + margin + ')'
+            })
+            .call(d3.axisLeft(yScale))
+
+        chart.selectAll('g.y-axis g.tick')
+            .append('line')
+            .classed('grid-line', true)
+            .attr('x1', 0)
+            .attr('y1', 0)
+            .attr('x2', (width - 2 * margin))
+            .attr('y2', 0)
+            .style('stroke-width', 1)
+            .style('stroke', '#ccc')
+
+
+    })()
+
+
+    ; (function () {
+
+        var xScale = d3.scaleLinear()
+            .domain([0, 100])
+            .range([0, 500]);
+
+        var xAxis = d3.axisBottom(xScale)
+            .ticks(10)
+            .tickPadding(10)
+            .tickFormat(function (d) { return d + "%"; })
+        d3.select('body').append('svg').attr('width', 800).append('g').attr('transform', 'translate(10,0)', ).call(xAxis)
+    })()
+
+    ; (function () {
+        var chartdata = [410, 370, 330, 300, 270, 240, 220, 200, 180, 165, 150, 135, 120,
+            135, 150, 165, 180, 200, 220, 240, 270, 300, 330, 370, 410]
+        var height = 200,
+            width = 800,
+            barWidth = width / chartdata.length - 2,
+            barOffset = 2
+
+        var xScale = d3.scaleOrdinal()
+            .domain(d3.range(0, chartdata.length))
+            .range([0, width])
+
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(chartdata)])
+            .range([0, height - 5])
+
+        var colors = d3.scaleLinear()
+            .domain([0, chartdata.length * .33, chartdata.length * .66, chartdata.length])
+            .range(['#d6e9c6', '#bce8f1', '#faebcc', '#ebccd1'])
+
+        var dynamicColor = ''
+
+        var chart = d3.select('body')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .style('background', '#bce8f1')
+
+        function applyTransition(selection, ease, delay, height) {
+            selection.transition()
+                .ease(ease)
+                .delay(delay)
+                .duration(1000)
+                .attr("height", height)
+                .attr('y', 200 - height)
+        }
+
+        chart.selectAll('rect')
+            .data(chartdata)
+            .enter()
+            .append('rect')
+            .style('fill', function (d, i) {
+                return colors(i)
+            })
+            .style('stroke', '#31708f')
+            .style('stroke-width', '5')
+            .attr('width', barWidth)
+            .attr('height', 0)
+            .attr('x', function (d, i) {
+                return i * (barWidth + barOffset)
+            })
+            .attr('y', height)
+            .on('mouseover', function () {
+                // --- 保存颜色
+                dynamicColor = this.style.fill
+                d3.select(this)
+                    .style('fill', '#3c763d')
+            })
+            .on('mouseout', function () {
+                d3.select(this)
+                    .style('fill', dynamicColor)
+            })
+            .each(function (d, i) {
+                d3.select(this)
+                    .call(applyTransition, d3.easeCubic, i * 50, yScale(d), i)
+            })
+    })()
 
     ; (function () {
         var data = []
@@ -54,7 +192,7 @@ import * as d3 from 'd3'
         //     // .rangeRoundBands([0, width], .1)
         //     .range([0, width])
 
-        var x = d3.scaleBand()
+        var x = d3.scaleLinear()
             .domain([0, data.length])
             .range([0, width])
 
@@ -65,7 +203,7 @@ import * as d3 from 'd3'
 
         var chart = d3.select('body')
             .append('svg')
-            .attr('class', 'chart')
+            .classed('chart', true)
             .attr('width', width + margin.left + margin.right)
             .attr('height', height + margin.top + margin.bottom)
 
