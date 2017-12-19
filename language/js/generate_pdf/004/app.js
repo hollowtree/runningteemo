2b499937737f6c81e1c13ab543355ed74fb8554b
hollowtree <hollowtree@gmx.cn>
2017-12-20 00:02:45 +0800

    generate pdf

--- language/js/generate_pdf/004/app.js
+++ language/js/generate_pdf/004/app.js
@@ -1,16 +1,28 @@
-const puppeteer = require('puppeteer');
+const fs = require('fs')
+const Koa = require('koa')
+const KoaBodyParser = require('koa-bodyparser')
+const KoaRouter = require('koa-router')()
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/', { waitUntil: 'networkidle' });
-    await page.pdf({
-        path: 'hn.pdf',
-        format: 'A4',
-        printBackground: true,
-        displayHeaderFooter: true,
-        margin: {top: 40, bottom: 40}
-    });
 
-    await browser.close();
-})();
+
+const routerController = require('./router')
+
+const app = new Koa()
+
+app.use(async (ctx, next) => {
+    // console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
+    fs.appendFileSync('request.log', `${new Date(+new Date()+8*3600*1000).toISOString()} Process ${ctx.request.method} ${ctx.request.url}`)
+    await next()
+})
+
+
+app.use(KoaBodyParser())
+
+KoaRouter.get('/generate_pdf_from_url', routerController.generatePdfFromUrl())
+
+app.use(KoaRouter.routes())
+
+const server = app.listen(3000, function () {
+    console.log('Server running on http://localhost:3000/')
+    console.log(`This process's pid is ${process.pid}`);
+})
