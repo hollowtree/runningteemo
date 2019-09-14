168d2f62a180747b16c1843ccbd5926d8d9a4a12
hollowtree <hollowtree@users.noreply.github.com>
2019-09-14 12:39:45 +0800

    go

--- language/go/001/main.go
+++ language/go/001/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt" 
+	"os"
+)
 
 func main(){
-	fmt.Println("Hello World!")
+	var s, sep string
+	for i := 0;i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
